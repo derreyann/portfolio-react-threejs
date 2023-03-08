b31bd8ad0c8d2a4118ca58f1a956901c6ed99a1d
Yann Derré <yann.derre44@gmail.com>
2023-03-08 17:58:02 +0100

    quick typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import App from "./App";
-//mport studio from "@theatre/studio";
+//import studio from "@theatre/studio";
 //import extension from "@theatre/r3f/dist/extension";
 import { createRoot } from "react-dom/client";
 
