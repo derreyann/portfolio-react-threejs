6da5e391120535804b1102709c69b94f9a7c68df
Yann Derré <yann.derre44@gmail.com>
2023-07-23 18:23:26 +0200

    removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { Suspense, useRef, useState, useEffect } from "react";
 import { Canvas, useFrame, extend } from "@react-three/fiber";
 import { easing } from "maath";
 
-import HeroPage from './HeroPage';
-import 'pepjs';
+import HeroPage from "./HeroPage";
+import "pepjs";
 import {
   OrbitControls,
   Stage,
@@ -17,14 +17,14 @@ import {
   RenderTexture,
   Loader,
   Html,
-  Sparkles
+  Sparkles,
 } from "@react-three/drei";
 import {
   TextureLoader,
   MeshBasicMaterial,
   CanvasTexture,
   RepeatWrapping,
-  Color
+  Color,
 } from "three";
 import {
   EffectComposer,
@@ -33,23 +33,20 @@ import {
   Selection,
   Select,
   Outline,
-  Noise
+  Noise,
 } from "@react-three/postprocessing";
 import { Model } from "./Model";
 import { getProject } from "@theatre/core";
 import { editable as e, SheetProvider, PerspectiveCamera } from "@theatre/r3f";
-import * as THREE from "three"
-// our Theatre.js project sheet, we'll use this later
+import * as THREE from "three";
 const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
 export default function App() {
   const ref = useRef();
   const [movableEnabled, setMovableEnabled] = useState(true);
 
-
   const [selectedMeshName, setSelectedMeshName] = useState("");
   const onMeshClick = (mesh) => {
     setSelectedMeshName(mesh.name);
-    //console.log(mesh.name);
   };
   return (
     <>
@@ -60,20 +57,17 @@ export default function App() {
         height="128"
         dpr={[0.3, 0.6]}
       >
-        <color attach="background" args={["black"]} />      <Suspense fallback={null}>
+        <color attach="background" args={["black"]} />
+        <Suspense fallback={null}>
           <SheetProvider sheet={demoSheet}>
             <PerspectiveCamera
               theatreKey="Camera"
               makeDefault
               position={[
-                -1.4235082949646687,
-                0.20562504834542494,
-                12.610933351075456
+                -1.4235082949646687, 0.20562504834542494, 12.610933351075456,
               ]}
               rotation={[
-                0.3788855445285419,
-                -0.05488506456904811,
-                -0.008547619429015501
+                0.3788855445285419, -0.05488506456904811, -0.008547619429015501,
               ]}
               fov={40}
               resolution={128}
@@ -83,11 +77,8 @@ export default function App() {
             <e.spotLight
               theatreKey="Light"
               position={[
-                -7.321508953834595,
-                134.55811607486405,
-                -41.339696743147556
+                -7.321508953834595, 134.55811607486405, -41.339696743147556,
               ]}
-
               angle={0.12}
               penumbra={1}
               intensity={1}
@@ -96,28 +87,39 @@ export default function App() {
             />
             <Bounds observe damping={3.5} margin={0.9}>
               <Selection>
-                <SelectToZoom setMovableEnabled={setMovableEnabled} movableEnabled={movableEnabled}>
-                  <Model SelectToZoom={onMeshClick} movableEnabled={movableEnabled} />
+                <SelectToZoom
+                  setMovableEnabled={setMovableEnabled}
+                  movableEnabled={movableEnabled}
+                >
+                  <Model
+                    SelectToZoom={onMeshClick}
+                    movableEnabled={movableEnabled}
+                  />
                 </SelectToZoom>
               </Selection>
             </Bounds>
             <AdaptiveDpr pixelated />
-            <Sparkles position={[0,4,0]} count={200} scale={10} size={0.85} speed={0.1} color={'#eeeeee'} />
-            <EffectComposer resolutionScale={0.10} disableNormalPass>
+            <Sparkles
+              position={[0, 4, 0]}
+              count={200}
+              scale={10}
+              size={0.85}
+              speed={0.1}
+              color={"#eeeeee"}
+            />
+            <EffectComposer resolutionScale={0.1} disableNormalPass>
               <Bloom
                 luminanceThreshold={0}
                 mipmapBlur
                 luminanceSmoothing={1.0}
                 intensity={3.5}
-
               />
               <DepthOfField
                 target={[0.52, 4.04, -6.91]}
-                focalLength={0.020}
+                focalLength={0.02}
                 bokehScale={10}
                 height={500}
               />
-
               <Noise opacity={0.035} />
             </EffectComposer>
           </SheetProvider>
@@ -131,7 +133,7 @@ export default function App() {
 
 function Movable() {
   const [enabled, setEnabled] = useState(true);
-  const target = new THREE.Vector3(0, 2, 0); //
+  const target = new THREE.Vector3(0, 2, 0);
   const [landscapeMode, setLandscapeMode] = useState(getLandscapeMode());
 
   useFrame(() => {
@@ -141,25 +143,24 @@ function Movable() {
     function handleResize() {
       setLandscapeMode(getLandscapeMode());
     }
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('orientationchange', handleOrientationChange);
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("orientationchange", handleOrientationChange);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('orientationchange', handleOrientationChange);
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("orientationchange", handleOrientationChange);
     };
   }, []);
 
   function getLandscapeMode() {
-    const mql = window.matchMedia('(orientation: landscape)');
+    const mql = window.matchMedia("(orientation: landscape)");
     return mql.matches;
   }
   const dampingFactor = landscapeMode ? 6 : 0.8;
   const rotatedampingFactor = landscapeMode ? 100 : 15;
 
-  //console.log(landscapeMode);
   useFrame((state, delta) => {
-    if (!enabled) return; // if not enabled, skip the parallax
+    if (!enabled) return;
 
     if (landscapeMode) {
       easing.damp3(
@@ -167,157 +168,76 @@ function Movable() {
         [
           -1 + (state.pointer.x * state.viewport.width) / dampingFactor,
           (2 + state.pointer.y) / 4,
-          7
+          7,
         ],
         0.5,
         delta
       );
-      easing.damp3(state.camera.rotation, [
-        0.3788855445285419,
-        -0.05488506456904811,
-        -0.008547619429015501
-      ], 0.5, delta);
-    }
-    else {
+      easing.damp3(
+        state.camera.rotation,
+        [0.3788855445285419, -0.05488506456904811, -0.008547619429015501],
+        0.5,
+        delta
+      );
+    } else {
       const direction = navigator.maxTouchPoints ? 1 : -1;
       easing.damp3(
         state.camera.rotation,
         [
           0,
-          (direction * state.pointer.x * state.viewport.width) * Math.PI / rotatedampingFactor,
-          0
+          (direction * state.pointer.x * state.viewport.width * Math.PI) /
+            rotatedampingFactor,
+          0,
         ],
         0.98,
         delta
       );
-      easing.damp3(state.camera.position,
-        [
-          0,
-          (5 + state.pointer.y * state.viewport.height / 10),
-          5
-        ],
+      easing.damp3(
+        state.camera.position,
+        [0, 5 + (state.pointer.y * state.viewport.height) / 10, 5],
         0.5,
         delta
       );
     }
   });
-  return null; // Movable doesn't need to return any elements
+  return null;
 }
-// This component wraps children in a group with a click handler
-// Clicking any object will refresh and fit bounds
+
 function SelectToZoom({ children, setMovableEnabled, movableEnabled }) {
   const api = useBounds();
   var i = 0;
   const [selectedObjectName, setSelectedObjectName] = useState("");
-  //console.log(children.type);
   const [selectedMeshName, setSelectedMeshName] = useState("");
   const [movableEnabled2, setMovableEnabled2] = useState(true);
   const [ObjectTexture, setObjectTexture] = useState("");
   const onMeshClick = (mesh) => {
     setSelectedMeshName(mesh.name);
-    //console.log(mesh.name);
   };
   return (
     <group
       onClick={(e) => {
-        //console.log(e.object.name);
         e.stopPropagation();
         setObjectTexture(e.object.material);
         setSelectedMeshName(e.object);
-        //console.log(e.object);
-        i++
-        //console.log(e);
+        i++;
         if (
           (e.object.type === "Mesh" && e.object.name.includes("Plane")) ||
           e.object.name.includes("named")
         ) {
-          //console.log("Yes!");
           api.refresh(e.object).fit();
 
           setMovableEnabled2(false);
-          /*
-          if (e.object.name === "named") {
-            if (i === 1 && !movableEnabled2) {
-              window.open(
-                "mailto:yann.derre@utbm.fr",
-                "_blank"
-              );
-            }
-            i++;
-            //console.log(i);
-          }
-          */
-
           if (e.object.name === "Plane1") {
-            //console.log("yepee");
-            /* 
-            const canvas = document.createElement("canvas");
-            canvas.width = 256;
-            canvas.height = 256;
-
-            // Render the HTML onto the canvas
-            const ctx = canvas.getContext("2d");
-            ctx.font = "50px serif";
-            ctx.fillStyle = "white";
-            ctx.font = "20px Arial";
-            ctx.fillText("Hello world", 40, 20);
-            console.log(ctx);
-            // Create a texture from the canvas
-            const texture = new CanvasTexture(canvas);
-            texture.wrapS = RepeatWrapping;
-            texture.repeat.x = -1;
-            texture.offset.x = -0.4;
-            console.log(texture);
-            
-            setObjectTexture(e.object.material);
-            setSelectedMeshName(e.object); 
-            // Render the HTML as a texture on the plane
-            //const texture = new TextureLoader().load(`data:text/html;charset=utf-8,${escape(html)}`);
-
-            e.object.material = new MeshBasicMaterial({
-              map: texture
-            });
-            console.log(e.object.material);
-
-            e.object.material.needsUpdate = true;
-            */
           }
         } else {
-          //console.log("Quit!");
-          /*selectedMeshName.material = ObjectTexture;
-          selectedMeshName.material.needsUpdate = true;
-          */
           setMovableEnabled2(true);
-          //setMovableEnabled(true);
           e.button === 0;
           i = 0;
-          //console.log(i);
         }
       }}
     >
       {children}
-      {movableEnabled2 && <Movable />} // conditionally render the Movable
-      component
+      {movableEnabled2 && <Movable />}
     </group>
   );
 }
-
-/*
-    <>
-          <group
-      onClick={(e) => {
-        e.stopPropagation();
-        if (e.object.type === "Mesh" && e.object.name.includes("Plane")) {
-          console.log("Yes!")
-          api.refresh(e.object).fit();
-        }
-      }}
-      onPointerMissed={(e) => e.button === 0}
-    >
-
-        {children}
-      </group>
-    </>
-  );
-}
-*/
