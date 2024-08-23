3653cde19d54f8801c988c7cf58edb4faa1ec243
Yann Derré <yann.derre44@gmail.com>
2024-08-23 11:14:27 +0200

    back button update

--- src/returnButton.jsx
+++ src/returnButton.jsx
@@ -1,29 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./ReturnButton.css";
 
 const ReturnButton = ({ onClick }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Trigger the fade-in effect when the component is mounted
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 500); // Delay to ensure the element has been mounted
+  }, []);
+
   return (
     <button
       onClick={onClick}
-      style={{
-        position: "fixed", // Use fixed positioning to keep it in the same place on the screen
-        bottom: "20px", // Distance from the bottom of the screen
-        left: "50%", // Center horizontally
-        transform: "translateX(-50%)", // Adjust for button width
-        padding: "10px 20px",
-        width: "100px",
-        height: "50px",
-        cursor: "pointer",
-        fontFamily: "monospace",
-        backgroundColor: "white",
-        userSelect: "none",
-        color: "black",
-        border: "none",
-        borderRadius: "100px",
-        WebkitTouchCallout: "none",
-        WebkitUserSelect: "none",
-        transition: "opacity 0.5s ease", // Fade transition
-        zIndex: 1000, // Ensure it's above other elements
-      }}
+      className={`blur-button ${isVisible ? "visible" : ""}`}
     >
       Back
     </button>
